8a06cff4283def5cef09dcb554abf7f752ef9b86
cicd <agonzalorodrigo@rocketmail.com>
2021-05-17 23:23:05 -0300

    publisher

--- vars/demo1local.groovy
+++ vars/demo1local.groovy
@@ -43,7 +43,56 @@ def call(body) {
             }
         }
         stage('Publicar'){
-            sh "mvn deploy:deploy-file -DgeneratePom=false -Dversion=${VERSION} -DgroupId=${GROUP} -DartifactId=${IMAGE} -DrepositoryId=nexus -Durl=${pipelineParams.nexusrepo} -Dfile=target/${IMAGE}-${VERSION}.${PACKAGE} -DuniqueVersion=false"
+            //sh "mvn deploy:deploy-file -DgeneratePom=false -Dversion=${VERSION} -DgroupId=${GROUP} -DartifactId=${IMAGE} -DrepositoryId=nexus -Durl=${pipelineParams.nexusrepo} -Dfile=target/${IMAGE}-${VERSION}.${PACKAGE} -DuniqueVersion=false"
+            // Read POM xml file using 'readMavenPom' step , this step 'readMavenPom' is included in: https://plugins.jenkins.io/pipeline-utility-steps
+            // This can be nexus3 or nexus2
+            NEXUS_VERSION = "nexus3"
+            // This can be http or https
+            NEXUS_PROTOCOL = "http"
+            // Where your Nexus is running
+            NEXUS_URL = "nexus-cicd.router.default.svc.cluster.local"
+            // Repository where we will upload the artifact
+            NEXUS_REPOSITORY = "maven-releases"
+            // Jenkins credential id to authenticate to Nexus OSS
+            NEXUS_CREDENTIAL_ID = "nexus-credentials"
+                    pom = readMavenPom file: "pom.xml";
+                    // Find built artifact under target folder
+                    filesByGlob = findFiles(glob: "target/*.${pom.packaging}");
+                    // Print some info from the artifact found
+                    echo "${filesByGlob[0].name} ${filesByGlob[0].path} ${filesByGlob[0].directory} ${filesByGlob[0].length} ${filesByGlob[0].lastModified}"
+                    // Extract the path from the File found
+                    artifactPath = filesByGlob[0].path;
+                    // Assign to a boolean response verifying If the artifact name exists
+                    artifactExists = fileExists artifactPath;
+                    if(artifactExists) {
+                        echo "*** File: ${artifactPath}, group: ${pom.groupId}, packaging: ${pom.packaging}, version ${pom.version}";
+
+                        nexusArtifactUploader(
+                            nexusVersion: NEXUS_VERSION,
+                            protocol: NEXUS_PROTOCOL,
+                            nexusUrl: NEXUS_URL,
+                            groupId: pom.groupId,
+                            version: pom.version,
+                            repository: NEXUS_REPOSITORY,
+                            credentialsId: NEXUS_CREDENTIAL_ID,
+                            artifacts: [
+                                // Artifact generated such as .jar, .ear and .war files.
+                                [artifactId: pom.artifactId,
+                                classifier: '',
+                                file: artifactPath,
+                                type: pom.packaging],
+
+                                // Lets upload the pom.xml file for additional information for Transitive dependencies
+                                [artifactId: pom.artifactId,
+                                classifier: '',
+                                file: "pom.xml",
+                                type: "pom"]
+                            ]
+                        );
+
+                    } else {
+                        error "*** File: ${artifactPath}, could not be found";
+                    }
         }
     }
 }
