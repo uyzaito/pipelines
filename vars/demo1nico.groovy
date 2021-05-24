22542d926d20efe48cf96392ec603ac9f0b40026
Nicolas Sosa <nicolas.sosa@atos.net>
2021-05-24 16:16:24 -0300

    Update demo1nico.groovy

--- vars/demo1nico.groovy
+++ vars/demo1nico.groovy
@@ -34,9 +34,10 @@ def call(body) {
                     sh "mvn test -f pom.xml"
                 }
                 stage('Analisis Sonarqube',) {
+                    def mvnHome = tool name: 'maven-3', type: 'maven'
                     withSonarQubeEnv ('sonar4.6') {
                         echo " SONAR GOAL --- $SONAR_MAVEN_GOAL"
-                        sh 'mvn org.sonarsource.scanner.maven:sonar-maven-plugin:4.6.2.2472:sonar'
+                        sh "${mvnHome}/bin/mvn sonar:sonar"
                     }
                 }
             }
