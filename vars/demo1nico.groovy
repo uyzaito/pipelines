2d0bf37e435e6b12bfdcfe4787ba8326157393db
Nicolas Sosa <nicolas.sosa@atos.net>
2021-05-25 15:15:56 -0300

    Update demo1nico.groovy

--- vars/demo1nico.groovy
+++ vars/demo1nico.groovy
@@ -57,7 +57,7 @@ def call(body) {
                         echo "*** File: ${artifactPath}, group: ${pom.groupId}, packaging: ${pom.packaging}, version ${pom.version}";
                         nexusArtifactUploader(
                             nexusVersion: NEXUS_VERSION,
-                          //  protocol: NEXUS_PROTOCOL,
+                            protocol: NEXUS_PROTOCOL,
                             nexusUrl: NEXUS_URL,
                             groupId: pom.groupId,
                             version: pom.version,
