e09f6a6ac880c01bdbfa6405d0d7f17be05cb27b
Nicolas Sosa <nicolas.sosa@atos.net>
2021-05-26 15:14:12 -0300

    Update demo1nico.groovy

--- vars/demo1nico.groovy
+++ vars/demo1nico.groovy
@@ -34,10 +34,9 @@ def call(body) {
                     sh "mvn test -f pom.xml"
                 }
                 stage('Analisis Sonarqube',) {
-                    withSonarQubeEnv ('sonarqube') {
-                        println ${env.SONAAR_HOST_URL}
-//                        echo " SONAR GOAL --- $SONAR_MAVEN_GOAL"
-                        sh "mvn $SONAAR_HOST_URL -DskipTests=true"
+                    withSonarQubeEnv {
+                        echo " SONAR GOAL --- $SONAR_MAVEN_GOAL"
+                        sh "mvn $SONAR_MAVEN_GOAL -DskipTests=true"
                     }
                 }
             }
