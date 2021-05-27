6bfb59ded796e6f75864ef58290c13a1b2e07daa
Nicolas Sosa <nicolas.sosa@atos.net>
2021-05-27 16:02:45 -0300

    Update demo1nico.groovy

--- vars/demo1nico.groovy
+++ vars/demo1nico.groovy
@@ -36,7 +36,7 @@ def call(body) {
                 stage('Analisis Sonarqube',) {
                     def scannerHome = tool 'sonarScanner';
                         withSonarQubeEnv("sonarServer") {
-                        sh mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.2:sonar-scanner -X \
+                        sh "mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.2:sonar-scanner -X \
                         -Dsonar.projectKey=com.yourcompany.newapp:honest-corn \
                         -Dsonar.sources=. \
                         -Dsonar.css.node=. \
