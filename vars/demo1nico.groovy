9b19a5c50092a7bb1779a9f0708123e37f3e2cff
cicd <agonzalorodrigo@rocketmail.com>
2021-05-31 16:27:11 -0300

    oc whoami

--- vars/demo1nico.groovy
+++ vars/demo1nico.groovy
@@ -84,8 +84,8 @@ def call(body) {
             sh """
                 mkdir -p ocp/deployments
                 cp target/${IMAGE}-${VERSION}.${PACKAGE} ocp/deployments/
-                oc project ${pipelineParams.ambiente}
                 oc whoami
+                oc project ${pipelineParams.ambiente}
                 oc new-build --binary=true --name=${IMAGE} --image-stream=redhat-openjdk18-openshift
                 oc start-build ${IMAGE} --from-dir=./ocp --follow
                 oc new-app ${IMAGE}
