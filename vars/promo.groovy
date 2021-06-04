be00837974ba9e14c5456b49fd9ff45e79571fc4
Gonzalo Alvarez <agonzalorodrigo@rocketmail.com>
2021-06-04 18:05:33 -0300

    USERINPUT

--- vars/promo.groovy
+++ vars/promo.groovy
@@ -15,56 +15,53 @@ def call(body) {
         stage ('Aceptacion de promocion') {
             try {
                 timeout(time:7, unit:'DAYS') {
-                    def userInput = input(
+                    userInput = input(
                         id: 'userInput',
                         message: "¿ APRUEBA LA EL DESPLIEGUE DE ${IMAGE} VERSION ${VERSION} EN EL AMBIENTE ${pipelineParams.ambiente} ?",
-                        parameters: [[$class: 'BooleanParameterDefinition', defaultValue: false, description: '', name: 'acepta']]                        
                     )
-                    echo "----- userInput.acepta $userInput.acepta"
                 }    
             } catch (error) {
                 echo "Error con input de aceptación----- error" 
             }        
         }
-        if ( userInput.acepta == true ) { 
-            stage('Promocion'){
-                openshift.withCluster() {
-                    openshift.withProject("${pipelineParams.ambiente}") {
-                        def imageStreamSelector = openshift.selector("dc","${IMAGE}")
-                        def imageStreamExists = imageStreamSelector.exists()
-                        if(!imageStreamExists) {
-                            echo "No existe la imagen ${IMAGE} en el ambiente ${pipelineParams.ambiente}"                 
-                            sh """
-                                oc project ${pipelineParams.ambiente}
-                                oc new-app ${IMAGE}
-                                oc expose svc/${IMAGE}
-                            """
-                            openshiftDeploy(depCfg: "${IMAGE}:${VERSION}", namespace: "${pipelineParams.ambiente}", waitTime: '10', waitUnit: 'min')
-                        }else{
-                            echo "Si existe la imagen ${IMAGE} en el ambiente ${pipelineParams.ambiente}"
-                            openshiftDeploy(depCfg: "${IMAGE}:${VERSION}", namespace: "${pipelineParams.ambiente}", waitTime: '10', waitUnit: 'min')
-                        }
-                    }                
-                }            
-            }
-            stage ('Disparador') {            
-                build(
-                    job: "${pipelineParams.tareaHija}",
-                    wait: false,
-                    parameters: [
-                            [
-                                    $class: 'StringParameterValue',
-                                    name: 'IMAGE',
-                                    value: "${IMAGE}",
-                            ],
-                            [
-                                    $class: 'StringParameterValue',
-                                    name: 'VERSION',
-                                    value: "${VERSION}",
-                            ],
-                    ],
-                )            
-            }
+        stage('Promocion'){
+            openshift.withCluster() {
+                openshift.withProject("${pipelineParams.ambiente}") {
+                    def imageStreamSelector = openshift.selector("dc","${IMAGE}")
+                    def imageStreamExists = imageStreamSelector.exists()
+                    if(!imageStreamExists) {
+                        echo "No existe la imagen ${IMAGE} en el ambiente ${pipelineParams.ambiente}"                 
+                        sh """
+                            oc project ${pipelineParams.ambiente}
+                            oc new-app ${IMAGE}
+                            oc expose svc/${IMAGE}
+                        """
+                        openshiftDeploy(depCfg: "${IMAGE}:${VERSION}", namespace: "${pipelineParams.ambiente}", waitTime: '10', waitUnit: 'min')
+                    }else{
+                        echo "Si existe la imagen ${IMAGE} en el ambiente ${pipelineParams.ambiente}"
+                        openshiftDeploy(depCfg: "${IMAGE}:${VERSION}", namespace: "${pipelineParams.ambiente}", waitTime: '10', waitUnit: 'min')
+                    }
+                }                
+            }            
         }
+        stage ('Disparador') {            
+            build(
+                job: "${pipelineParams.tareaHija}",
+                wait: false,
+                parameters: [
+                        [
+                                $class: 'StringParameterValue',
+                                name: 'IMAGE',
+                                value: "${IMAGE}",
+                        ],
+                        [
+                                $class: 'StringParameterValue',
+                                name: 'VERSION',
+                                value: "${VERSION}",
+                        ],
+                ],
+            )            
+        }
+        
     }
 }
