64dbde7285490f342da0092ff7e580fd2947a02f
Gonzalo Alvarez <agonzalorodrigo@rocketmail.com>
2021-06-07 12:01:10 -0300

    ++

--- vars/promo.groovy
+++ vars/promo.groovy
@@ -39,7 +39,8 @@ def call(body) {
                         """
                     }else{
                         echo "Si existe la imagen ${IMAGE} en el ambiente ${pipelineParams.ambiente}"
-                        openshiftDeploy(depCfg: "${IMAGE}:${VERSION}", namespace: "${pipelineParams.ambiente}", waitTime: '10', waitUnit: 'min')
+                        sh "oc deploy ${IMAGE}:${VERSION}"
+                        //openshiftDeploy(depCfg: "${IMAGE}:${VERSION}", namespace: "${pipelineParams.ambiente}", waitTime: '10', waitUnit: 'min')
                     }
                 }                
             }            
